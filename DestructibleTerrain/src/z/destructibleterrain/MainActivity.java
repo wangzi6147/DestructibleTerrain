fee42859166b08b1e3694a2690d3ee341dbd2a64
wangzi6147 <363177773@qq.com>
2014-09-25 22:55:59 +0800

    1、整理代码；2、添加多个测试小球。

--- DestructibleTerrain/src/z/destructibleterrain/MainActivity.java
+++ DestructibleTerrain/src/z/destructibleterrain/MainActivity.java
@@ -1,13 +1,28 @@
 package z.destructibleterrain;
 
+import java.util.ArrayList;
+
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.collision.shapes.EdgeShape;
 import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 
+import com.seisw.util.geom.Poly;
+import com.seisw.util.geom.PolyDefault;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -20,18 +35,159 @@ public class MainActivity extends Activity {
 
 	private myView myView;
 
+	private Bitmap bitmap;
+
+	private PolyDefault terrainPolygon;
+
+	private BodyDef bodyDef;
+
+	private Body body;
+
+	private FixtureDef fixtureDef;
+
+	private EdgeShape shape;
+
+	private ArrayList<Body> balls = new ArrayList<Body>();
+
+	private PolyDefault explosionPolygon;
+	// 小球之间间隔
+	protected int timeInterBalls = 100;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// 去标题
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		world = new World(new Vec2(0, 10f));
-		myView = new myView(this, world);
+		myView = new myView(this);
+		// 地图图片
+		bitmap = BitmapFactory.decodeResource(getResources(),
+				R.drawable.testpic);
+		myView.setBitmap(bitmap);
+		// 地图边缘
+		setTerrain();
+
 		setContentView(myView);
 		handler = new Handler();
 		handler.post(update);
 
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		// 模拟爆炸多边形
+		explosionPolygon = createCircle(20, new Point((int) event.getX(),
+				(int) event.getY()), 60);
+		// 多边形运算：模拟破坏地图效果
+		terrainPolygon = (PolyDefault) terrainPolygon
+				.difference(explosionPolygon);
+		// 先创建出新地图刚体，再把原地图刚体摧毁
+		createEdgeBody(terrainPolygon);
+		Body oriBody = findBody("oriTerrain");
+		if (oriBody != null) {
+			world.destroyBody(findBody("oriTerrain"));
+		}
+		return super.onTouchEvent(event);
+	}
+
+	private PolyDefault createCircle(int polyNum, Point centerPoint, int radius) {
+		// TODO Auto-generated method stub
+		double angle = 2 * Math.PI / polyNum;
+		PolyDefault circlePolygon = new PolyDefault();
+		for (int i = 0; i < polyNum; i++) {
+			int x = (int) (centerPoint.x + radius * Math.cos(angle * i));
+			int y = (int) (centerPoint.y + radius * Math.sin(angle * i));
+			circlePolygon.add(x, y);
+		}
+		return circlePolygon;
+	}
+
+	public Body createTestBall(float ballX, float ballY, float radius) {
+		// TODO Auto-generated method stub
+		CircleShape ballShape = new CircleShape();
+		ballShape.setRadius(radius);
+		FixtureDef def = new FixtureDef();
+		def.restitution = 0.3f;
+		def.friction = 0.2f;
+		def.density = 1f;
+		def.shape = ballShape;
+		BodyDef ballDef = new BodyDef();
+		ballDef.type = BodyType.DYNAMIC;
+		ballDef.position.set(ballX, ballY);
+
+		Body ballBody = world.createBody(ballDef);
+		ballBody.createFixture(def);
+		ballBody.setUserData("ball");
+
+		return ballBody;
+	}
+
+	private void setTerrain() {
+		// TODO Auto-generated method stub
+		terrainPolygon = new PolyDefault();
+		terrainPolygon.add(500, 200);
+		terrainPolygon.add(1500, 200);
+		terrainPolygon.add(1500, 1200);
+		terrainPolygon.add(500, 1200);
+		// 初始化地图刚体
+		createEdgeBody(terrainPolygon);
+	}
+
+	private void createEdgeBody(Poly terrainPoly) {
+		// TODO Auto-generated method stub
+
+		// 刷新刚体时，需要把原刚体信息更新
+		Body oriBody = findBody("newTerrain");
+		if (oriBody != null) {
+			oriBody.setUserData("oriTerrain");
+		}
+
+		// 新建地图边缘刚体
+		bodyDef = new BodyDef();
+		bodyDef.position.set(0, 0);
+		bodyDef.type = BodyType.STATIC;
+		body = world.createBody(bodyDef);
+		body.setUserData("newTerrain");
+
+		fixtureDef = new FixtureDef();
+		shape = new EdgeShape();
+		for (int i = 0; i < terrainPoly.getNumInnerPoly(); i++) {
+
+			int numPoints = terrainPoly.getInnerPoly(i).getNumPoints();
+			createShortEdge(terrainPoly.getInnerPoly(i), numPoints - 1, 0);
+			for (int j = 0; j < numPoints - 1; j++) {
+				// 创建小段刚体
+				createShortEdge(terrainPoly.getInnerPoly(i), j, j + 1);
+			}
+		}
+	}
+
+	private Body findBody(String string) {
+		// TODO Auto-generated method stub
+		Body body = world.getBodyList();
+		while (body != null) {
+			if (body.getUserData() == string) {
+				return body;
+			}
+			body = body.getNext();
+		}
+		return null;
+	}
+	
+	private void createShortEdge(Poly innerPoly, int pointNum_1, int pointNum_2) {
+		// TODO Auto-generated method stub
+		Vec2 v1 = new Vec2((float) innerPoly.getX(pointNum_1) / Constants.RATE,
+				(float) innerPoly.getY(pointNum_1) / Constants.RATE);
+		Vec2 v2 = new Vec2((float) innerPoly.getX(pointNum_2) / Constants.RATE,
+				(float) innerPoly.getY(pointNum_2) / Constants.RATE);
+
+		shape.set(v1, v2);
+		fixtureDef.shape = shape;
+		body.createFixture(fixtureDef);
 	}
 
 	private Runnable update = new Runnable() {
@@ -39,9 +195,20 @@ public class MainActivity extends Activity {
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
+			// 测试小球
+			if(timeInterBalls==100){
+				balls.add(createTestBall(1000f / Constants.RATE,
+						10f / Constants.RATE, 20f / Constants.RATE));
+				timeInterBalls = 0;
+			}
+			timeInterBalls++;
+			//设置Box2D世界迭代
 			world.step(1f / 60f, 10, 8);
+			//传递绘制信息
+			myView.setBalls(balls);
+			myView.setTerrainPolygon(terrainPolygon);
 			myView.invalidate();
-			handler.postDelayed(update, 100 / 6);
+			handler.postDelayed(update, 10);
 		}
 	};
 
--- DestructibleTerrain/src/z/destructibleterrain/myView.java
+++ DestructibleTerrain/src/z/destructibleterrain/myView.java
@@ -1,5 +1,7 @@
 package z.destructibleterrain;
 
+import java.util.ArrayList;
+
 import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.collision.shapes.EdgeShape;
 import org.jbox2d.common.Vec2;
@@ -24,87 +26,28 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class myView extends View {
-	
 
 	private PolyDefault terrainPolygon;
-	private PolyDefault explosionPolygon;
 	private Bitmap bitmap;
-	private World world;
-	private BodyDef bodyDef;
-	private FixtureDef fixtureDef;
-	private Body ballBody;
-	private EdgeShape shape;
-	private Body body;
+	private ArrayList<Body> balls;
 
-	public myView(Context context, World world) {
+	public myView(Context context) {
 		super(context);
-		this.world = world;
 		// TODO Auto-generated constructor stub
-		setTerrain();
-		bitmap = BitmapFactory.decodeResource(getResources(),
-				R.drawable.testpic);
-		createTestBall(1000f / Constants.RATE, 10f / Constants.RATE, 20f / Constants.RATE);
-	}
-
-	public void createTestBall(float ballX, float ballY, float radius) {
-		// TODO Auto-generated method stub
-		CircleShape ballShape = new CircleShape();
-		ballShape.setRadius(radius);
-		FixtureDef def = new FixtureDef();
-		def.restitution = 0.3f;
-		def.friction = 0.2f;
-		def.density = 1f;
-		def.shape = ballShape;
-		BodyDef ballDef = new BodyDef();
-		ballDef.type = BodyType.DYNAMIC;
-		ballDef.position.set(ballX, ballY);
-
-		ballBody = world.createBody(ballDef);
-		ballBody.createFixture(def);
-		ballBody.setUserData("ball");
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-
-		explosionPolygon = createCircle(20, new Point((int) event.getX(),
-				(int) event.getY()), 60);
-		terrainPolygon = (PolyDefault) terrainPolygon
-				.difference(explosionPolygon);
-		// 先创建出新地图刚体，再把原地图刚体摧毁
-		createEdgeBody(terrainPolygon);
-		Body oriBody = findBody("oriTerrain");
-		if (oriBody != null) {
-			world.destroyBody(findBody("oriTerrain"));
-
-		}
-		return super.onTouchEvent(event);
-	}
-
-	private void setTerrain() {
-		// TODO Auto-generated method stub
-		terrainPolygon = new PolyDefault();
-		terrainPolygon.add(500, 200);
-		terrainPolygon.add(1500, 200);
-		terrainPolygon.add(1500, 1200);
-		terrainPolygon.add(500, 1200);
-		//初始化地图刚体
-		createEdgeBody(terrainPolygon);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		// TODO Auto-generated method stub
 		super.onDraw(canvas);
-
+		// 绘制测试小球
 		drawBall(canvas);
-		for (int i = 0; i < terrainPolygon.getNumInnerPoly(); i++) {
-
-			drawPolygon(terrainPolygon.getInnerPoly(i), canvas);
+		for (int i = 0; i < getTerrainPolygon().getNumInnerPoly(); i++) {
+			// 绘制地图
+			drawPolygon(getTerrainPolygon().getInnerPoly(i), canvas);
 
 		}
 
-		
 	}
 
 	private void drawBall(Canvas canvas) {
@@ -112,61 +55,13 @@ public class myView extends View {
 		Paint mPaint = new Paint();
 		mPaint.setAntiAlias(true);
 		mPaint.setColor(Color.GREEN);
-		canvas.save();
-		canvas.drawCircle(ballBody.getPosition().x * Constants.RATE,
-				ballBody.getPosition().y * Constants.RATE, 20, mPaint);
-		canvas.restore();
-	}
+		for (int i = 0; i < balls.size(); i++) {
 
-	private void createEdgeBody(Poly terrainPoly) {
-		// TODO Auto-generated method stub
-		Body oriBody = findBody("newTerrain");
-		if (oriBody != null) {
-			oriBody.setUserData("oriTerrain");
-
+			canvas.save();
+			canvas.drawCircle(balls.get(i).getPosition().x * Constants.RATE,
+					balls.get(i).getPosition().y * Constants.RATE, 20, mPaint);
+			canvas.restore();
 		}
-
-		bodyDef = new BodyDef();
-		bodyDef.position.set(0, 0);
-		bodyDef.type = BodyType.STATIC;
-		body = world.createBody(bodyDef);
-		body.setUserData("newTerrain");
-
-		fixtureDef = new FixtureDef();
-		shape = new EdgeShape();
-		for (int i = 0; i < terrainPoly.getNumInnerPoly(); i++) {
-
-			int numPoints = terrainPoly.getInnerPoly(i).getNumPoints();
-			createShortEdge(terrainPoly.getInnerPoly(i), numPoints - 1, 0);
-			for (int j = 0; j < numPoints - 1; j++) {
-				createShortEdge(terrainPoly.getInnerPoly(i), j, j + 1);
-
-			}
-		}
-	}
-
-	private Body findBody(String string) {
-		// TODO Auto-generated method stub
-		Body body = world.getBodyList();
-		while (body != null) {
-			if (body.getUserData() == string) {
-				return body;
-			}
-			body = body.getNext();
-		}
-		return null;
-	}
-
-	private void createShortEdge(Poly innerPoly, int pointNum_1, int pointNum_2) {
-		// TODO Auto-generated method stub
-		Vec2 v1 = new Vec2((float) innerPoly.getX(pointNum_1) / Constants.RATE,
-				(float) innerPoly.getY(pointNum_1) / Constants.RATE);
-		Vec2 v2 = new Vec2((float) innerPoly.getX(pointNum_2) / Constants.RATE,
-				(float) innerPoly.getY(pointNum_2) / Constants.RATE);
-
-		shape.set(v1, v2);
-		fixtureDef.shape = shape;
-		body.createFixture(fixtureDef);
 	}
 
 	private void drawPolygon(Poly poly, Canvas canvas) {
@@ -180,6 +75,7 @@ public class myView extends View {
 			path.lineTo(point.x, point.y);
 		}
 		path.close();
+		// 如果当前多边形是空洞，涂为背景色
 		if (poly.isHole()) {
 			Paint paint = new Paint();
 			paint.setColor(Color.WHITE);
@@ -188,20 +84,33 @@ public class myView extends View {
 		} else {
 			canvas.save();
 			canvas.clipPath(path);
-			canvas.drawBitmap(bitmap, 0, 0, null);
+			canvas.drawBitmap(getBitmap(), 0, 0, null);
 			canvas.restore();
 		}
 	}
 
-	private PolyDefault createCircle(int polyNum, Point centerPoint, int radius) {
-		// TODO Auto-generated method stub
-		double angle = 2 * Math.PI / polyNum;
-		PolyDefault circlePolygon = new PolyDefault();
-		for (int i = 0; i < polyNum; i++) {
-			int x = (int) (centerPoint.x + radius * Math.cos(angle * i));
-			int y = (int) (centerPoint.y + radius * Math.sin(angle * i));
-			circlePolygon.add(x, y);
-		}
-		return circlePolygon;
+	public Bitmap getBitmap() {
+		return bitmap;
 	}
+
+	public void setBitmap(Bitmap bitmap) {
+		this.bitmap = bitmap;
+	}
+
+	public PolyDefault getTerrainPolygon() {
+		return terrainPolygon;
+	}
+
+	public void setTerrainPolygon(PolyDefault terrainPolygon) {
+		this.terrainPolygon = terrainPolygon;
+	}
+
+	public ArrayList<Body> getBalls() {
+		return balls;
+	}
+
+	public void setBalls(ArrayList<Body> balls) {
+		this.balls = balls;
+	}
+
 }
